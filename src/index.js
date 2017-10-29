c50e61795007c3f199a9c7b9300349041a83ab03
Rahul Kushwaha <kushwaharesonance@gmail.com>
2017-10-29 16:08:19 +0530

    created post form

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import promise from 'redux-promise';
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/posts/new/" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+
+        </Switch>
+    </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
