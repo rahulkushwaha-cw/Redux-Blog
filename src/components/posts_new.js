63a2d9eacae33a48a61377c86577fe3bf88c3ec0
Rahul Kushwaha <kushwaharesonance@gmail.com>
2017-10-30 08:34:23 +0530

    created post component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,22 +1,32 @@
 import React,{Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createPost} from '../actions/index';
 class PostsNew extends Component{
   renderField(field){
+    const {meta:{touched, error}} = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
     return(
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.touched ? field.meta.error : ''}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
+
       </div>
     );
   }
 
 onSubmit(values){
-  console.log(values);
+  this.props.createPost(values, () =>{
+    this.props.history.push('/');
+  });
 }
   render(){
     const {handleSubmit} = this.props;
@@ -38,7 +48,7 @@ onSubmit(values){
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
-
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
@@ -59,4 +69,6 @@ function validate(values){
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null,{createPost})(PostsNew)
+);
---
+++ src/components/posts_show.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions';
+
+class PostsShow extends Component{
+  componentDidMount(){
+    const {id} = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+  render(){
+    return(
+      <div>
+        Posts Show
+      </div>
+    );
+  }
+}
+function mapStateToProps({posts}){
+
+}
+export default connect(null,{fetchPost})(PostsShow);
